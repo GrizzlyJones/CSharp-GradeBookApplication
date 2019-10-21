315e5d7c90a9220d26aff50acf92a3255f320eae
GrizzlyJones <j.hornholt@gmail.com>
2019-10-21 15:33:23 +0200

    Completed module

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradeBook : BaseGradeBook
+    {
+        public RankedGradeBook(global::System.String name) : base(name)
+        {
+            Type = GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradeBook : BaseGradeBook
+    {
+        public StandardGradeBook(global::System.String name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
